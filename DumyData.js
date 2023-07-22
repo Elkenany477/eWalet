f1d8d0dddc7797ca827eae13a57cdb85cea5f124
Elkenany <abdelrahmanelkenany62@gmail.com>
2023-07-22 14:15:56 -0800

    Upload eWalet app

---
+++ DumyData.js
@@ -0,0 +1,42 @@
+export const Services = [
+    {
+        id: 1,
+        Name: "Send Money",
+        image: require('./img/Send.png'),
+    },
+    {
+        id: 1,
+        Name: "Receive Money",
+        image: require('./img/Recieve.png')
+    },
+    {
+        id: 1,
+        Name: "Mobile prepaid",
+        image: require('./img/Prepaid.png')
+    },
+    {
+        id: 1,
+        Name: "Electricity Bill",
+        image: require('./img/Bill.png')
+    },
+    {
+        id: 1,
+        Name: "Cashback Offer",
+        image: require('./img/Offer.png')
+    },
+    {
+        id: 1,
+        Name: "Movie Tickets",
+        image: require('./img/Tickets.png')
+    },
+    {
+        id: 1,
+        Name: "Flight Tickets",
+        image: require('./img/plane.png')
+    },
+    {
+        id: 1,
+        Name: "More Options",
+        image: require('./img/Option.png')
+    },
+]
